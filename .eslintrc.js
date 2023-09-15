1382350ad2c0a670a480527dfe838ceebeca057f
sor4chi <sorakawa0218@gmail.com>
2023-09-15 11:53:18 +0900

    chore: init

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const { resolve } = require("node:path");
+
+const project = resolve(process.cwd(), "tsconfig.json");
+
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "unused-imports"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.*"],
+};
