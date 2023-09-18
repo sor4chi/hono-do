186596bfc303b17a1f807193403c36614e8fad14
sor4chi <sorakawa0218@gmail.com>
2023-09-18 17:15:38 +0900

    feat: implement simple Hono Object

--- packages/hono-do/src/index.ts
+++ packages/hono-do/src/index.ts
@@ -1,7 +1,23 @@
 import { Hono } from "hono";
 
-export class HonoObject extends Hono {
-  constructor() {
-    super();
-  }
+interface HonoObject {
+  state: DurableObjectState;
+  app: Hono;
+}
+
+export function generateHonoObject(
+  basePath: string,
+  cb: (app: Hono, state: DurableObjectState) => void,
+) {
+  const honoObject = function (this: HonoObject, state: DurableObjectState) {
+    const app = new Hono().basePath(basePath);
+    this.app = app;
+    cb(app, state);
+  };
+
+  honoObject.prototype.fetch = function (this: HonoObject, request: Request) {
+    return this.app.fetch(request);
+  };
+
+  return honoObject;
 }
