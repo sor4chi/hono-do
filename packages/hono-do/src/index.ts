4b9c753fbecb37f742489eeb394d20d2181a0e40
sor4chi <sorakawa0218@gmail.com>
2023-09-19 05:36:58 +0900

    feat: extend Hono's generics env to HonoObject

--- packages/hono-do/src/index.ts
+++ packages/hono-do/src/index.ts
@@ -1,20 +1,34 @@
-import { Hono } from "hono";
+import { Env, Hono, Schema } from "hono";
 
-interface HonoObject {
-  app: Hono;
+interface HonoObject<
+  E extends Env = Env,
+  S extends Schema = Record<string, never>,
+  BasePath extends string = "/",
+> {
+  app: Hono<E, S, BasePath>;
 }
 
-export function generateHonoObject(
+export function generateHonoObject<
+  E extends Env = Env,
+  S extends Schema = Record<string, never>,
+  BasePath extends string = "/",
+>(
   basePath: string,
-  cb: (app: Hono, state: DurableObjectState) => void,
+  cb: (app: Hono<E, S, BasePath>, state: DurableObjectState) => void,
 ) {
-  const honoObject = function (this: HonoObject, state: DurableObjectState) {
-    const app = new Hono().basePath(basePath);
+  const honoObject = function (
+    this: HonoObject<E, S, BasePath>,
+    state: DurableObjectState,
+  ) {
+    const app = new Hono<E, S, BasePath>().basePath(basePath);
     this.app = app;
     cb(app, state);
   };
 
-  honoObject.prototype.fetch = function (this: HonoObject, request: Request) {
+  honoObject.prototype.fetch = function (
+    this: HonoObject<E, S, BasePath>,
+    request: Request,
+  ) {
     return this.app.fetch(request);
   };
 
