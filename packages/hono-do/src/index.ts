52a7f71417e07c434d701b6f6e06e23597c6e5ff
sor4chi <sorakawa0218@gmail.com>
2023-09-19 13:54:49 +0900

    refactor(index): refactor defineState function

--- packages/hono-do/src/index.ts
+++ packages/hono-do/src/index.ts
@@ -44,20 +44,19 @@ export async function defineState<T>(
     await storage.put(key, initialValue);
   }
 
-  return [
-    async () => {
-      return (await storage.get<T>(key)) || initialValue;
-    },
-    async (value: T | ((value: T) => T)) => {
-      if (typeof value === "function") {
-        const newValue = (value as (value: T) => T)(
-          (await storage.get<T>(key)) || initialValue,
-        );
-        await storage.put(key, newValue);
-        return newValue;
-      }
-      await storage.put(key, value);
-      return value;
-    },
-  ];
+  const get = async () => {
+    return (await storage.get<T>(key)) || initialValue;
+  };
+
+  const set = async (value: T | ((value: T) => T)) => {
+    if (typeof value === "function") {
+      const newValue = (value as (value: T) => T)(await get());
+      await storage.put(key, newValue);
+      return newValue;
+    }
+    await storage.put(key, value);
+    return value;
+  };
+
+  return [get, set];
 }
