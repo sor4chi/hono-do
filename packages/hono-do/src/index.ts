899efcd2600c3c207a677b5deea4eeed407551fa
sor4chi <sorakawa0218@gmail.com>
2023-09-27 03:09:28 +0900

    chore: export state helper as sub package

--- packages/hono-do/src/index.ts
+++ packages/hono-do/src/index.ts
@@ -39,29 +39,3 @@ export function generateHonoObject<
 
   return honoObject;
 }
-
-export async function defineState<T>(
-  storage: DurableObjectStorage,
-  key: string,
-  initialValue: T,
-): Promise<[() => Promise<T>, (value: T | ((value: T) => T)) => Promise<T>]> {
-  if (!(await storage.get(key))) {
-    await storage.put(key, initialValue);
-  }
-
-  const get = async () => {
-    return (await storage.get<T>(key)) || initialValue;
-  };
-
-  const set = async (value: T | ((value: T) => T)) => {
-    if (typeof value === "function") {
-      const newValue = (value as (value: T) => T)(await get());
-      await storage.put(key, newValue);
-      return newValue;
-    }
-    await storage.put(key, value);
-    return value;
-  };
-
-  return [get, set];
-}
