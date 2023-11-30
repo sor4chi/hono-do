eb768e013c3e6f23c22aa346fbccbb0f605a0ff5
sor4chi <sorakawa0218@gmail.com>
2023-12-01 00:12:59 +0900

    fix: rewrite the example using Hibernatiable Websocket API

--- examples/hibernatable-chat/src/chat.ts
+++ examples/hibernatable-chat/src/chat.ts
@@ -8,14 +8,19 @@ function uuidv4() {
   });
 }
 
-export const Chat = generateHonoObject("/chat", (app) => {
-  const messages: {
-    timestamp: string;
-    text: string;
-  }[] = [];
-  const sessions = new Map<string, WebSocket>();
+declare module "hono-do" {
+  interface HonoObjectVars {
+    messages: {
+      timestamp: string;
+      text: string;
+    }[];
+  }
+}
 
-  app.get("/messages", async (c) => c.json(messages));
+export const Chat = generateHonoObject("/chat", (app, state, vars) => {
+  vars.messages = [];
+
+  app.get("/messages", async (c) => c.json(vars.messages));
 
   app.get("/websocket", async (c) => {
     if (c.req.header("Upgrade") === "websocket") {
@@ -27,30 +32,27 @@ export const Chat = generateHonoObject("/chat", (app) => {
   async function handleWebSocketUpgrade() {
     const [client, server] = Object.values(new WebSocketPair());
     const clientId = uuidv4();
-    server.accept();
+    state.acceptWebSocket(server);
 
-    sessions.set(clientId, server);
-
-    server.addEventListener("message", (msg) => {
-      if (typeof msg.data !== "string") return;
-      messages.push(JSON.parse(msg.data));
-      broadcast(msg.data, clientId);
-    });
+    server.serializeAttachment({ clientId });
 
     return new Response(null, { status: 101, webSocket: client });
   }
+});
 
-  function broadcast(message: string, senderClientId?: string) {
-    for (const [clientId, webSocket] of sessions.entries()) {
-      if (clientId === senderClientId) {
-        continue;
-      }
-
-      try {
-        webSocket.send(message);
-      } catch (error) {
-        sessions.delete(clientId);
-      }
+Chat.webSocketMessage(async (webSocket, msg, state, vars) => {
+  const { clientId: senderClientId } = await webSocket.deserializeAttachment();
+  state.getWebSockets().forEach((ws) => {
+    const { clientId } = ws.deserializeAttachment();
+    if (clientId === senderClientId) {
+      return;
     }
-  }
+
+    try {
+      vars.messages.push(JSON.parse(msg.toString()));
+      ws.send(msg.toString());
+    } catch (error) {
+      ws.close();
+    }
+  });
 });
