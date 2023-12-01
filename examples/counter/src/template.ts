547c3380508ac7ef044cf448f66e48ac21af1e91
sor4chi <sorakawa0218@gmail.com>
2023-12-02 01:49:28 +0900

    fix: use hono/html for template string

--- examples/counter/src/template.ts
+++ examples/counter/src/template.ts
@@ -1,28 +1,31 @@
-export const Template = /*html*/ `
-<html>
-  <body>
-    <h1>Counter</h1>
-    <p>Current value: <span id="value"></span></p>
-    <button id="increment">Increment</button>
-    <button id="decrement">Decrement</button>
-    <script>
-      const value = document.getElementById("value");
-      const increment = document.getElementById("increment");
-      const decrement = document.getElementById("decrement");
-      const updateValue = async () => {
-        const res = await fetch("/counter");
-        value.innerText = await res.text();
-      }
-      increment.addEventListener("click", async () => {
-        await fetch("/counter/increment", { method: "POST" });
-        await updateValue();
-      })
-      decrement.addEventListener("click", async () => {
-        await fetch("/counter/decrement", { method: "POST" });
-        await updateValue();
-      })
-      updateValue();
-    </script>
-  </body>
-</html>
-`.trim();
+import { html } from "hono/html";
+
+export const Template = html`
+  <!doctype html>
+  <html>
+    <body>
+      <h1>Counter</h1>
+      <p>Current value: <span id="value"></span></p>
+      <button id="increment">Increment</button>
+      <button id="decrement">Decrement</button>
+      <script>
+        const value = document.getElementById("value");
+        const increment = document.getElementById("increment");
+        const decrement = document.getElementById("decrement");
+        const updateValue = async () => {
+          const res = await fetch("/counter");
+          value.innerText = await res.text();
+        };
+        increment.addEventListener("click", async () => {
+          await fetch("/counter/increment", { method: "POST" });
+          await updateValue();
+        });
+        decrement.addEventListener("click", async () => {
+          await fetch("/counter/decrement", { method: "POST" });
+          await updateValue();
+        });
+        updateValue();
+      </script>
+    </body>
+  </html>
+`;
