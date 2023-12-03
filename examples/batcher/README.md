1045ed9493a2ec92625974113e3623ca00544a1d
sor4chi <sorakawa0218@gmail.com>
2023-12-03 19:57:52 +0900

    feat(example): extend batcher examples to use bindings (D1)

--- examples/batcher/README.md
+++ examples/batcher/README.md
@@ -5,6 +5,25 @@ The code is based on [Cloudflare's Alarm example](https://developers.cloudflare.
 
 ```
 pnpm install
-pnpm dev
 ```
 
+Setup the database:
+
+```
+pnpm create-db
+```
+
+Copy the outputted database configuration.
+Add the database configuration to the `wrangler.toml` file.
+
+Add the following to the `wrangler.toml` file's `[[d1_databases]]` section:
+
+```
+preview_database_id = "DB"
+```
+
+Run the following to start the batcher:
+
+```
+pnpm dev
+```
--- examples/batcher/src/batcher.ts
+++ examples/batcher/src/batcher.ts
@@ -2,6 +2,8 @@
 
 import { generateHonoObject } from "hono-do";
 
+import { Env } from ".";
+
 const SECONDS = 1000;
 
 declare module "hono-do" {
@@ -10,29 +12,42 @@ declare module "hono-do" {
   }
 }
 
-export const Batcher = generateHonoObject(
-  "/batcher",
-  async (app, state, vars) => {
-    const { storage } = state;
-    const vals = await storage.list({ reverse: true, limit: 1 });
-    vars.count = vals.size === 0 ? 0 : parseInt(vals.keys().next().value);
+export const Batcher = generateHonoObject<
+  Env,
+  Record<string, never>,
+  "/batcher"
+>("/batcher", async (app, state, vars) => {
+  await vars.env.DB.prepare(
+    "CREATE TABLE IF NOT EXISTS batcher (id INTEGER PRIMARY KEY AUTOINCREMENT, data TEXT)",
+  ).run();
 
-    app.post("/", async (c) => {
-      vars.count++;
-
-      const currentAlarm = await storage.getAlarm();
-      if (currentAlarm == null) {
-        await storage.setAlarm(Date.now() + 10 * SECONDS);
-      }
-
-      await storage.put(vars.count.toString(), await c.req.text());
-      return c.json({ queued: vars.count });
-    });
-  },
-).alarm(async (state, vars) => {
   const { storage } = state;
-  const vals = await storage.list();
-  console.log(Array.from(vals.values())); // eg: POST other upstream service
+  const vals = await storage.list<string>({ reverse: true, limit: 1 });
+  vars.count = vals.size === 0 ? 0 : parseInt(vals.keys().next().value);
+
+  app.post("/", async (c) => {
+    vars.count++;
+
+    const currentAlarm = await storage.getAlarm();
+    if (currentAlarm == null) {
+      await storage.setAlarm(Date.now() + 10 * SECONDS);
+    }
+
+    await storage.put(vars.count.toString(), await c.req.text());
+    return c.json({ queued: vars.count });
+  });
+}).alarm(async ({ storage }, vars) => {
+  const vals = await storage.list<string>();
+  let query = "INSERT INTO batcher (data) VALUES ";
+  const params: string[] = [];
+  for (const value of vals.values()) {
+    query += "(?),";
+    params.push(value);
+  }
+  query = query.slice(0, -1);
+  await vars.env.DB.prepare(query)
+    .bind(...params)
+    .run();
   await storage.deleteAll();
   vars.count = 0;
 });
